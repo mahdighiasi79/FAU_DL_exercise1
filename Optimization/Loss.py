4412c946c2674dfafca96d77532589e54e1bab42
Mahdi Ghiasi <mmghf74@gmail.com>
2024-05-23 19:43:40 +0100

    cross entropy loss implemented

--- Optimization/Loss.py
+++ Optimization/Loss.py
@@ -4,13 +4,14 @@ import numpy as np
 class CrossEntropyLoss:
 
     def __init__(self):
-        return
+        self.prediction_tensor = np.array([])
 
-    @staticmethod
-    def forward(prediction_tensor, label_tensor):
+    def forward(self, prediction_tensor, label_tensor):
+        self.prediction_tensor = prediction_tensor
         loss = label_tensor * np.log(prediction_tensor)
         loss = -np.sum(loss, keepdims=False)
         return loss
 
     def backward(self, label_tensor):
-        pass
+        error_tensor = (-np.log(np.e) * label_tensor) / self.prediction_tensor
+        return error_tensor
