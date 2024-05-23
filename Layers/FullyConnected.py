70d173cbe8d2ca87c371447d4e2be9d8a1f4d875
Mahdi Ghiasi <mmghf74@gmail.com>
2024-05-23 16:18:42 +0100

    ReLU implemented

--- Layers/FullyConnected.py
+++ Layers/FullyConnected.py
@@ -9,8 +9,20 @@ class FullyConnected(Base.BaseLayer):
         self.trainable = True
         self.weights = np.random.uniform(0, 1, input_size * output_size).reshape((input_size, output_size))
         self.biases = np.zeros(output_size)
+        self._optimizer = "sgd"
 
     def forward(self, input_tensor):
         output_tensor = input_tensor @ self.weights
         output_tensor += self.biases
         return output_tensor
+
+    def get_optimizer(self):
+        return self._optimizer
+
+    def set_optimizer(self, value):
+        self._optimizer = value
+
+    optimizer = property(get_optimizer, set_optimizer)
+
+    def backward(self, error_tensor):
+        pass
---
+++ Layers/ReLU.py
@@ -0,0 +1,18 @@
+import Base
+
+
+class ReLU(Base.BaseLayer):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(input_tensor):
+        output_tensor = input_tensor > 0
+        output_tensor *= input_tensor
+        return output_tensor
+
+    @staticmethod
+    def backward(error_tensor):
+        relu_derivative = error_tensor > 0
+        return error_tensor * relu_derivative
