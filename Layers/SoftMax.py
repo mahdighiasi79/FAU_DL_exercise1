softmax and cross entropy loss functions' bugs fixed

Layers/SoftMax.py
@@ -1,5 +1,6 @@
 # import Base
 import numpy as np
+import copy
 
 
 class SoftMax:
@@ -19,11 +20,12 @@ class SoftMax:
     def backward(self, error_tensor):
         batch_size, output_size = self.output_tensor.shape
         derivative_matrix = np.repeat(self.output_tensor[:, :, np.newaxis], output_size, axis=2)
-        transpose_matrix = derivative_matrix.transpose((0, 2, 1))
+        transpose_matrix = copy.deepcopy(derivative_matrix.transpose((0, 2, 1)))
         transpose_matrix *= -derivative_matrix
         identity_matrix = np.identity(output_size)
         identity_matrix = np.repeat(identity_matrix[np.newaxis, :, :], batch_size, axis=0)
         derivative_matrix = transpose_matrix + (identity_matrix * derivative_matrix)
-        derivative_matrix = np.sum(derivative_matrix, axis=1, keepdims=False)
+        error_tensor = np.repeat(error_tensor[:, :, np.newaxis], output_size, axis=2)
         error_tensor *= derivative_matrix
+        error_tensor = np.sum(error_tensor, axis=1, keepdims=False)
         return error_tensor